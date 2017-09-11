a1bee7394e08af36b280b733fe6173870dc84f71
Ayberk Anil ATSIZ <anil@houseofapps.com>
2017-09-11 09:50:50 +0300

    font family added

--- ios/RNMeasureText.m
+++ ios/RNMeasureText.m
@@ -21,40 +21,49 @@ RCT_EXPORT_METHOD(measure:(NSDictionary *)options
                   resolver:(RCTPromiseResolveBlock)resolve
                   rejecter:(RCTPromiseRejectBlock)reject)
 {
+    UIFont *font = nil;
+    
     if ([options objectForKey:@"width"] == nil) {
-      reject(@"invalid_width", @"missing required width property", nil);
-      return;
+        reject(@"invalid_width", @"missing required width property", nil);
+        return;
     }
     if ([options objectForKey:@"texts"] == nil) {
-      reject(@"invalid_texts", @"missing required texts property", nil);
-      return;
+        reject(@"invalid_texts", @"missing required texts property", nil);
+        return;
     }
     if ([options objectForKey:@"fontSize"] == nil) {
-      reject(@"invalid_fontSize", @"missing required fontSize property", nil);
-      return;
+        reject(@"invalid_fontSize", @"missing required fontSize property", nil);
+        return;
     }
-
+    
+    CGFloat fontSize = [RCTConvert CGFloat:options[@"fontSize"]];
+    
+    if([options objectForKey:@"fontFamily"] == nil){
+        font = [UIFont systemFontOfSize: fontSize];
+    }else{
+        font = [UIFont fontWithName:options[@"fontFamily"] size:fontSize];
+    }
+    
     float width = [RCTConvert float:options[@"width"]];
     NSArray *texts = [RCTConvert NSArray:options[@"texts"]];
-    CGFloat fontSize = [RCTConvert CGFloat:options[@"fontSize"]];
-
+    
     NSMutableArray* results = [[NSMutableArray alloc] init];
-    UIFont *font = [UIFont systemFontOfSize: fontSize];
-
+    
+    
     for (NSString* text in texts) {
         NSTextStorage *textStorage = [[NSTextStorage alloc] initWithString:text];
         NSTextContainer *textContainer = [[NSTextContainer alloc] initWithSize: CGSizeMake(width, FLT_MAX)];
         NSLayoutManager *layoutManager = [[NSLayoutManager alloc] init];
-
+        
         [layoutManager addTextContainer:textContainer];
         [textStorage addLayoutManager:layoutManager];
-
+        
         [textStorage addAttribute:NSFontAttributeName value:font
                             range:NSMakeRange(0, [textStorage length])];
         [textContainer setLineFragmentPadding:0.0];
         (void) [layoutManager glyphRangeForTextContainer:textContainer];
         CGRect resultRect = [layoutManager usedRectForTextContainer:textContainer];
-
+        
         [results addObject:[NSNumber numberWithFloat:resultRect.size.height]];
     }
     resolve(results);
